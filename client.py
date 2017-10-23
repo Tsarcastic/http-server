3f0ab530b321fc209b860156a1125cb0b6a19b26
Brendan Davis <brendanmd@gmail.com>
2017-10-23 13:43:45 -0700

    windows sucks

--- client.py
+++ client.py
@@ -1,2 +1,16 @@
 """Client portion of the exercise on day 6 of Python 401."""
 import socket
+
+def main():
+    info = socket.getaddrinfo('127.0.0.1', 5000)
+    print(len(info[0][4]))
+    print(info[0][4])
+    print(type(info))
+    for i in info:
+        print(i)
+    stream_info = [i for i in info if i == socket.SOCK_STREAM][0]
+    #client = socket.socket(*stream_info[:3])
+    #client.connect(stream_info[-1])
+
+if __name__ == '__main__':
+    main()
