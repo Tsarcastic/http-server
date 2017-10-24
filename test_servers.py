6d7cea6e83fc4eb3ea979cba8204490cf3564354
Matt Favoino <mattfavoino@gmail.com>
2017-10-24 14:56:22 -0700

    Added text for equivalence of 200 response

--- test_servers.py
+++ test_servers.py
@@ -2,19 +2,29 @@
 import pytest
 
 
-def test_send_and_receive_echo():
-    """Send an echo to server and hear an echo back."""
-    from client import client
-    assert client('echo echo echo') == 'echo echo echo'
+# def test_send_and_receive_echo():
+#     """Send an echo to server and hear an echo back."""
+#     from client import client
+#     assert client('echo echo echo') == 'echo echo echo'
 
 
 def test_response_ok():
     """Test if response ok returns a HTTP200 message."""
     from server import response_ok
-    assert len(response_ok()) > 50
+    assert len(response_ok()) > 0
+
+
+def test_response_ok_200_message():
+    """Test to see if response string sent back equals expected message."""
+    from server import response_ok
+    expected_response = """HTTP/1.1 200 OK
+Content-Type: text/plain
+
+"""
+    assert response_ok() == expected_response.encode('utf8')
 
 
 def test_response_error():
     """Test response error function."""
     from server import response_error
-    assert len(response_error()) > 50
+    assert len(response_error()) > 0
